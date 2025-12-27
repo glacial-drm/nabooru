e5a7fde5d1a97334fd47b07fdb7af3a0d25ce33a
GlacialDreamss <danieladetokunbo05@gmail.com>
2025-12-27 16:58:08 +0000

    db file adding functionality, now branch

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 import PIL.Image
 
 # add stuff to db
-directory = "placeholder"
+directory = 'E:/Downloads/ref'
 
 # keep in file, make function in taskbar to configure... config file?
 supported_filetypes = ['gif', 'jpeg', 'jpg', 'png', 'webp', 'mp4', 'webm']
